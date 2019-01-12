07a79aeb1513567a9a5e130009a794903c85c464
Hayreddin Tüzel <hayreddintuzel@gmail.com>
2019-01-12 20:11:57 +0300

    some improvements

--- services/passport.js
+++ services/passport.js
@@ -17,8 +17,8 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: keys.gooogleClientID,
-    clientSecret: keys.gooogleClientSecret,
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 },
 (accessToken, refreshToken, profile, done) => {
