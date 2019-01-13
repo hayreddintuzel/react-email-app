44862f5b30033ba15c9945678c2c654ecf523595
Hayreddin Tüzel <hayreddintuzel@gmail.com>
2019-01-13 22:23:46 +0300

    login/logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 const p3 = {
     paddingLeft: '9px',
   };
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login with Google</a></li>
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
+
     render() {
+        console.log(this.props);
         return (
             <nav style={p3}>
                 <div className="nav-wrapper">
                     <a href="#" className="brand-logo">Emaily</a>
                     <ul id="nav-mobile" className="right">
-                        <li><a href="auth/google">Login With Google</a></li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -19,4 +32,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,16 +5,20 @@ module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google',{
         scope: ['profile', 'email']
     }))
-    
-    app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get('/auth/google/callback', passport.authenticate('google'),
+    (req,res) => {
+        res.redirect('/surveys');
+    }
+    );
 
     app.get('/api/logout', (req, res) => {
         req.logout();
-        res.send(req.user);
+        res.redirect('/');
     });
 
     app.get('/api/me', (req, res) => {
-        res.send(req.session);
+        res.send(req.user);
     });
 
 };
