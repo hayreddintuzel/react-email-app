0952c5c5b5f5dc503f3f8bbe391ee169a47e9d78
Hayreddin Tüzel <hayreddintuzel@gmail.com>
2019-01-13 22:26:58 +0300

    landing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,8 @@ import * as actions from '../actions';
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
+
+import Landing from './Landing';
 
 class App extends Component {
     componentDidMount() {
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>
+                Emaily!
+            </h1>
+            Colect feedback from your users
+        </div>
+    )
+}
+
+export default Landing;
