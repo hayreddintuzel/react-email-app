83caa678a5ec9327eda7651f3ca064856975ebd0
hayreddin tuzel <hayreddin.tuzel@osf-commerce.com>
2019-02-17 17:40:41 +0300

    before last section

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,8 @@
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
@@ -8,7 +11,39 @@ const Survey = mongoose.model('Surveys');
 
 module.exports = app => {
 
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+
+        const events = _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
+                if(match) {
+                    return {email, surveyId: match.surveyId, choice: match.choice};
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({surveyId, email, choice}) => {
+                Survey.updateOne(
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch: { email: email, responded: false}
+                        }
+                    },
+                    {
+                        $inc: { [choice]: 1},
+                        $set: { 'recipients.$.responded': true},
+                        lastResponded: new Date()
+                    }
+                ).exec();
+            })
+            .value();
+
+            res.send({});
+    });
+
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for contribution');
     });
 
@@ -38,10 +73,5 @@ module.exports = app => {
         }
 
     });
-
-    app.post('api/surveys/webhooks', (req, res) => {
-        res.send("dasda");
-    })
-
 
 };
